b03c70e5aedfb561e8d4bf103cf9cd89a0c50fb2
Francisco <franamuchastegui@gmail.com>
2019-11-10 20:48:50 -0300

    Rest server - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,20 @@
  * Puerto
  */
 process.env.PORT = process.env.PORT || 3000;
+
+// Entorno
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+/*
+ * Base de datos
+ */
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+	urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+	urlDB = 'mongodb+srv://fran:1of8aQ1PAcuih7BN@cluster0-k1dhu.mongodb.net/cafe';
+}
+
+
+process.env.URLDB = urlDB;
