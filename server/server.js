56dd89a79df77a6a417e675abe7a195904fc0608
Francisco <franamuchastegui@gmail.com>
2019-11-18 16:59:55 -0300

    Actualizo documentación

--- server/server.js
+++ server/server.js
@@ -17,26 +17,6 @@ app.use(bodyParser.json())
 // habilitar la carpeta public
 app.use(express.static(path.resolve(__dirname, '../public')));
 
-// Add headers
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
-
 // configuración global de rutas
 app.use(require('./routes/index'));
 
